33744d8e5258a8a345f4f66d383ed9b47c11ab59
Laura <laurallamasruiz@gmail.com>
2020-11-09 19:17:42 +0100

    Restringir movimiento, movimiento de cámara y crear columnas.

--- Zaxxon_GrupoB/Assets/Scripts/CamaraLook.cs
+++ Zaxxon_GrupoB/Assets/Scripts/CamaraLook.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class CamaraLook : MonoBehaviour
 {
     [SerializeField] Transform Tarjet;
-    
+    [SerializeField] float smoothVelocity = 0.3F;
+    [SerializeField] Vector3 camaraVelocity = Vector3.zero;
+    [SerializeField] Transform playerPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +19,7 @@ public class CamaraLook : MonoBehaviour
     void Update()
     {
         transform.LookAt(Tarjet);
+        Vector3 targetPosition = new Vector3(transform.position.x, playerPosition.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camaraVelocity, smoothVelocity);
     }
 }
